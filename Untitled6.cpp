0c6cf6e1af4221399fb9160a33bed0530285c2c5
Ali <arogan929@gmail.com>
2019-04-19 21:47:58 +0700

    Halo

---
+++ Untitled6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int nilai=37;
+	
+	cout<<nilai++<<endl;
+	cout<<nilai--<<endl;
+	cout<<++nilai<<endl;
+	cout<<nilai--<<endl;
+	cout<<--nilai<<endl;
+
+	return 0;
+}
