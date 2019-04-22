9089c3281054b0d1b18b76c45082fad1db521770
Ali <arogan929@gmail.com>
2019-04-22 09:11:51 +0700

    Belajar Branch v1

---
+++ andi nina.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main (){
+	float hasil;
+	int D;
+	const float phi = 3.14;
+	cout << "masukkan D : ";
+	cin >>D;
+	hasil = phi*D;
+	cout <<hasil;
+	
+	
+	
+}
